202ab43ea6eb32a77633ab84551927c8f3266730
joewatts000 <joewatts000@hotmail.com>
2018-01-20 13:30:39 +0000

    frontend build

---
+++ src/team.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	const teamBtns = document.querySelectorAll('.team-list li');
+
+	teamBtns.forEach(function(memberBtn){
+		memberBtn.addEventListener('click', showTeamInfo, {passive:true});
+	});
+
+	function showTeamInfo(e){
+		document.querySelector('.team-list li.active').classList.remove('active');
+		e.target.classList.add('active');
+		document.querySelector('.team-member-info.active').classList.remove('active');
+		document.getElementById(e.target.dataset.target).classList.add('active');
+	}	
+}
