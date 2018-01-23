2e502b75389f746874ac3129c41e44cca849ad50
joewatts000 <joewatts000@hotmail.com>
2018-01-23 14:42:24 +0000

    add work single page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ import slider from './js/slider.js';
 import menu from './js/menu.js';
 
 window.onload = function(){
-	team.init();
-	slider.init();
 	menu.init();
+
+	if(document.querySelector('.team-panel')){
+		team.init();
+	}
+
+	if(document.querySelector('.work-slider')){
+		slider.init();
+	}
+
+	const pageLinks = document.querySelectorAll('.pageLink');
+
+	pageLinks.forEach(function(link){
+		console.log('hey');
+		link.addEventListener('click', goToPage, {passive : false});
+	});
+
+	function goToPage(e){
+		console.log(e.target);
+		window.location.href = e.target.dataset.location + '.html';
+	}
 }
