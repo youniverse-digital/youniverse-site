d44ff0e4786b3e5b72116812d7151ff81dfd9aef
joewatts000 <joewatts000@hotmail.com>
2018-01-28 09:37:43 +0000

    add work pge

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './scss/main.scss';
 import team from './js/team.js';
 import slider from './js/slider.js';
 import menu from './js/menu.js';
+import headerAnim from './js/headerAnimation.js';
+import animations from 'create-keyframe-animation';
+
 
 window.onload = function(){
 	menu.init();
@@ -14,15 +17,48 @@ window.onload = function(){
 		slider.init();
 	}
 
+	if(document.querySelector('.animating-header')){
+		headerAnim.init();
+	}
+
+	// var animations = require('create-keyframe-animation')
+	const yVal = document.querySelector('.website-scroll').clientHeight;
+	const yDelta = document.querySelector('.phone-anim-phone').clientHeight;
+	// this creates the animation above
+	animations.registerAnimation({
+	  name: 'move',
+	  // the actual array of animation changes
+	  animation: [
+		[0,0], 
+		[0, -(yVal - yDelta)]
+	  ],
+	  // optional presets for when actually running the animation
+	  presets: {
+	    duration: 1000,
+	    easing: 'linear',
+	    delay: 500,
+	    resetWhenDone: true
+	  }
+	})
+
+	// then run it
+	const el = document.querySelector('.website-scroll');
+
+	animations.runAnimation(el, 'move', startAnim);
+
+	function startAnim(){
+		el.style.transform = '';
+		el.style.animation = '';
+		// animations.runAnimation(el, 'move', startAnim);
+	}
+
 	const pageLinks = document.querySelectorAll('.pageLink');
 
 	pageLinks.forEach(function(link){
-		console.log('hey');
 		link.addEventListener('click', goToPage, {passive : false});
 	});
 
 	function goToPage(e){
-		console.log(e.target);
 		window.location.href = e.target.dataset.location + '.html';
 	}
 }
---
+++ src/js/headerAnimation.js
@@ -0,0 +1,7 @@
+function init(){
+	document.querySelector('.animating-header').classList.add('animate');
+}
+
+module.exports = {
+	init : init
+}
