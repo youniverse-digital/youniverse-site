e6670b792ae3b228631238dfe6ffb93c941e7c35
Joe Watts <you@example.com>
2018-02-01 19:30:12 +0000

    update to playbook styles

--- src/assets/js/accordions.js
+++ src/assets/js/accordions.js
@@ -1,6 +1,5 @@
 function init(){
 	const accordions = document.querySelectorAll('.accordion h3');
-	console.log(accordions.length);
 
 	accordions.forEach(function(accordion){
 		accordion.addEventListener('click', changeAccordion, {passive : true});
@@ -8,16 +7,19 @@ function init(){
 }
 
 function changeAccordion(e){
-	if(document.querySelector('.accordion .content.active')){
-		document.querySelector('.accordion .content.active').classList.remove('active');
+
+	if(document.querySelector('.accordion .active') && !e.target.classList.contains('active')){
+		if(document.querySelector('.accordion .content.active')){
+			document.querySelector('.accordion .content.active').classList.remove('active');
+		}
+
+		if(document.querySelector('.accordion h3.active')){
+			document.querySelector('.accordion h3.active').classList.remove('active');
+		}
 	}
 
-	if(document.querySelector('.accordion h3.active')){
-		document.querySelector('.accordion h3.active').classList.remove('active');
-	}
-
-	e.target.nextElementSibling.classList.add('active');
-	e.target.classList.add('active');
+	e.target.nextElementSibling.classList.toggle('active');
+	e.target.classList.toggle('active');
 }
 
 module.exports = {
