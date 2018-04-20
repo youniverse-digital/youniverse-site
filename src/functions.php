69dd733f6e976f5cbdc64c6667097d38cbac6a64
Daniel Wildman <dan@Daniels-MacBook-Pro.local>
2018-04-20 13:28:55 +0100

    Updated functions file

--- src/functions.php
+++ src/functions.php
@@ -79,7 +79,7 @@ function html5blank_header_scripts()
      //    wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
      //    wp_enqueue_script('modernizr'); // Enqueue it!
 
-        wp_register_script('html5blankscripts', get_template_directory_uri() . './assets/js/app.bundle.js', array(), '1.0.0'); // Custom scripts
+        wp_register_script('html5blankscripts', get_template_directory_uri() . '/assets/js/app.bundle.js', array(), '1.0.0'); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
     }
 }
@@ -99,7 +99,7 @@ function html5blank_styles()
     // wp_register_style('normalize', get_template_directory_uri() . '/normalize.css', array(), '1.0', 'all');
     // wp_enqueue_style('normalize'); // Enqueue it!
 
-    wp_register_style('html5blank', get_template_directory_uri() . './assets/css/app.css', array(), '1.0', 'all');
+    wp_register_style('html5blank', get_template_directory_uri() . '/assets/css/app.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
 }
 
